7099eae3a79d33f3e80dfa5b8d624c5f5d44afcc
admin <admin@admins-MacBook-Pro.local>
2024-06-03 17:07:24 +0700

    update

--- src/main/java/com/poly/assignment/entity/KhachHang.java
+++ src/main/java/com/poly/assignment/entity/KhachHang.java
@@ -3,10 +3,12 @@ package com.poly.assignment.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
@@ -22,15 +24,15 @@ public class KhachHang {
     private Integer id;
 
     @Column(name = "ma_khach_hang")
-    @NotBlank(message = "Vui long nhap ma khach hang")
+    @NotBlank(message = "Vui lòng nhập mã khách hàng")
     private String maKH;
 
     @Column(name = "ten")
-    @NotBlank(message = "Vui long nhap ten khach hang")
+    @NotBlank(message = "Vui lòng nhập tên khách hàng")
     private String ten;
 
     @Column(name = "sdt")
-    @NotBlank(message = "Vui long nhap so dien thoai")
+    @Pattern(regexp = "^0[0-9]{9}+", message = "Nhập số điện thoại bắt đầu bằng 0, gồm 10 số và không chứa chữ")
     private String sdt;
 
     @Column(name = "trang_thai")
